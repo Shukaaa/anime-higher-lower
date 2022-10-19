3a0d12c76556e31ef049cd88d4921430b6563f6a
R\LU1668 <lucas-fabien.bernard@regiocom.com>
2022-10-19 16:58:19 +0200

    Woop Woop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ComponentModule } from './components/component.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent,
-    HeaderComponent
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
