1e8e2b8c96b0baffc2b919c1a351df80cee6c8ec
R\LU1668 <lucas-fabien.bernard@regiocom.com>
2022-10-21 16:57:20 +0200

    Working Game State

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -14,6 +14,7 @@ export class ContentComponent implements OnInit {
   randomAnime1: any
   randomAnime2: any
   randomAnime: any
+  score = 0
 
   // difficulty settings
   max_top_amount = 200
@@ -21,9 +22,50 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   async ngOnInit(): Promise<void> {
+    await this.viewControllFirst()
+    await this.loadNewGameData()
+    await this.viewControllSecond()
+  }
+
+  async chooseAnime(i: number) {
+    await this.viewControllFirst()
+
+    let j = 0
+    if (i == 0) {
+      j = 1
+    }
+
+    let chosenAnime = this.randomAnime[i]
+    let secondAnime = this.randomAnime[j]
+
+    if (chosenAnime.members >= secondAnime.members) {
+      this.score++
+      alert("You're right! New Score: " + this.score)
+      await this.loadNewGameData()
+      await this.viewControllSecond()
+    } else {
+      alert("You're wrong :(")
+      await this.loadNewGameData()
+      this.score = 0
+      await this.viewControllSecond()
+    }
+  }
+
+  async viewControllFirst() {
     // @ts-ignore
     document.getElementById("game").style.display = "none"
+    // @ts-ignore
+    document.getElementById("loading").style.display = "block"
+  }
 
+  async viewControllSecond() {
+    // @ts-ignore
+    document.getElementById("game").style.display = "block"
+    // @ts-ignore
+    document.getElementById("loading").style.display = "none"
+  }
+
+  async loadNewGameData() {
     this.randomAnime1 = await this.requests.getRandom("anime", this.max_top_amount)
     while (true) {
       this.randomAnime2 = await this.requests.getRandom("anime", this.max_top_amount)
@@ -36,12 +78,5 @@ export class ContentComponent implements OnInit {
     }
 
     this.randomAnime = [this.randomAnime1[0], this.randomAnime2[0]]
-
-    console.log(this.randomAnime)
-
-    // @ts-ignore
-    document.getElementById("game").style.display = "block"
-    // @ts-ignore
-    document.getElementById("loading").remove()
   }
 }
